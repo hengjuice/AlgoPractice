7acacbbe16d2b0a6d81ea391a8f94fc13f2b7bc1
Heng Jiu Xiao <66162052+hengjuice@users.noreply.github.com>
2020-12-24 21:32:02 +0800

    Add files via upload

---
+++ intrapolationSearch.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int interpolationSearch(int arr[], int n, int x){
+    int low = 0; 
+    int high = n-1;
+    while(low<=high && x >= arr[low] && x <= arr[high]){
+        if(low == high){
+            if(arr[low] == x)
+                return low;
+            return -1;
+        }
+        int pos = low + (((double)(high-low)/(arr[high]-arr[low])) * (x-arr[low]));
+        
+        if(arr[pos] == x)
+            return pos;
+            
+        if(arr[pos] < x)
+            low = pos+1;
+            
+        else
+            high = pos-1;
+    }
+    return -1;
+    
+}
+
+int main()
+{
+    // Array of items on which search will
+    // be conducted.
+    int arr[] = {10, 12, 13, 16, 18, 19, 20, 21,
+                 22, 23, 24, 33, 35, 42, 47};
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    int x = 18; // Element to be searched
+    int index = interpolationSearch(arr, n, x);
+ 
+    // If element was found
+    if (index != -1)
+        cout << "Element found at index " << index;
+    else
+        cout << "Element not found.";
+    return 0;
+}
